3fd3d8fd26f5c576f6e1d2864fdf33ea8220e0d9
kazancity <kazancity@aol.com>
2022-05-21 22:12:00 +0300

    feat: job 04-copy-directory completed (Sat, May 21, 2022 22:12:00 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,5 @@
-
+const{resolve}=require('path'),
+dir=resolve(__dirname),{rm,mkdir,copyFile,readdir}=require('fs').promises
+copy=async(from,to)=>{await rm(to,{recursive:true,force:true});await mkdir(to)
+for(item of await readdir(from))copyFile(resolve(from,item),resolve(to,item))}
+copy(dir+'\\files',dir+'\\files-copy')
