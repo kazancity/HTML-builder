4057f3ab694b8eed55de8d98b8e0d059adf26b0e
kazancity <kazancity@aol.com>
2022-05-24 20:23:30 +0300

    fix: fix path copy folder (Tue, May 24, 2022 20:23:30 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,5 +1,5 @@
 const{resolve}=require('path'),
-dir=resolve(__dirname),{rm,mkdir,copyFile,readdir}=require('fs').promises
+{rm,mkdir,copyFile,readdir}=require('fs').promises
 copy=async(from,to)=>{await rm(to,{recursive:true,force:true});await mkdir(to)
 for(item of await readdir(from))copyFile(resolve(from,item),resolve(to,item))}
 copy(resolve(__dirname,'files'),resolve(__dirname,'files-copy'));
