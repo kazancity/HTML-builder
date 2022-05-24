69cb0f27ade23dae0046eb15e6d58e013ffd979e
kazancity <kazancity@aol.com>
2022-05-24 20:48:00 +0300

    fix: fix path copy folder (Tue, May 24, 2022 20:48:00 PM)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,7 +1,6 @@
 const fs=require('fs'),
-fsProm=require('fs').promises,
-path=require('path'),dir=path.resolve(__dirname),
-bun=path.resolve(dir+'\\project-dist','bundle.css'),cws=fs.createWriteStream(bun);
-(async()=>{const files=await fsProm.readdir(dir+'\\styles',{withFileTypes:true})
- files.forEach(e=>{if(e.isFile()){curStl=path.resolve(dir+'\\styles',e.name)
+fsProm=require('fs').promises,path=require('path')
+bun=path.resolve(path.resolve(__dirname,'project-dist'),'bundle.css'),cws=fs.createWriteStream(bun);
+(async()=>{const files=await fsProm.readdir(path.resolve(__dirname,'styles'),{withFileTypes:true})
+ files.forEach(e=>{if(e.isFile()){curStl=path.resolve(path.resolve(__dirname,'styles'),e.name)
  if(path.extname(curStl)=='.css')fs.createReadStream(curStl).pipe(cws)}})})()
